559ea6adbeda63941d1efe55010207338616d36d
Denis Terekhin <denizterekhin@yandex.ru>
2025-04-13 17:38:52 +0300

    создан скрипт по 5 спринту, добавление карточек с данными из масива с готовым темплейтем и реализация их удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTmp = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(`.places__list`);
+const deletButton = document.querySelector('.card__delete-button');
 
 // @todo: Функция создания карточки
+function createCard(data) {
+  const placeItem = cardTmp.querySelector(`.places__item`).cloneNode(true); // Клонирование темплейта в константу
+  placeItem.querySelector(`.card__title`).textContent = data.name; // передача тайтла карточки в клонированый темплейт
+  placeItem.querySelector(`.card__image`).src = data.link; //передача ссылки на картинку в клонированый темплейт
+
+  placeItem.querySelector(`.card__delete-button`).addEventListener(`click`, () => handleDeleteCard(placeItem));
+
+
+  return placeItem;
+}
 
 // @todo: Функция удаления карточки
+function handleDeleteCard(item){
+  item.remove();
+}
+
+// @todo: функция вставки карточки на страницу
+function renderCard(card){
+  placesList.append(card);
+}
+
 
 // @todo: Вывести карточки на страницу
+ initialCards.forEach((card) => {
+  renderCard(createCard(card))
+ });
